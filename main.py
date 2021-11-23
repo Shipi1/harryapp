293b3e34f3476ceffa6899f5fab340de51b3557f
Shipi1 <Shipi1@users.noreply.github.com>
2021-11-23 12:07:23 -0300

    primeros archivos de prueba

---
+++ main.py
@@ -0,0 +1,15 @@
+#TODO;
+'''
+Funciones:
+-Notificaciones
+-Ajustes
+-Ingresar emociones
+-Diario
+-Guardar frases
+
+y después dejarlo bonito con UI y todo :)
+'''
+import django
+
+
+#def ingresarEmociones():
